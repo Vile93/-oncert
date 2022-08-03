36e4bb488dc1768bcc022d89f1f0153aca078fad
Vile <vilemusicyt@gmail.com>
2022-08-03 17:33:47 -0600

    finished adding @media

---
+++ js/hamburger.js
@@ -0,0 +1,49 @@
+const hamburger = document.querySelector('.hamburger'),
+      menuItem = document.querySelectorAll('.menu__item'),
+      menuLink = document.querySelectorAll('.menu__link'),
+      header = document.getElementById('header');
+
+let overflow = true;
+
+const bodyOverflow = () => {
+    if (overflow) {
+        body.style.overflow = "hidden";
+        overflow = false;
+    } else {
+        body.style.overflow = "scroll";
+        overflow = true;
+    }
+}
+
+const checkClass = () => {
+    if (currClick.classList.contains('menu__link')) {
+        if (!(currClick.dataset.link == 1)) {
+            hamburger.classList.toggle('hamburger--active');
+            menuItem.forEach((el, ind) => {
+                menuItem[ind].classList.toggle('menu__item--active');
+            });
+            bodyOverflow();
+        }
+    }
+}
+
+hamburger.addEventListener('click', e => {
+    hamburger.classList.toggle('hamburger--active');
+    menuItem.forEach((el, ind) => {
+        menuItem[ind].classList.toggle('menu__item--active');
+    });
+    bodyOverflow();
+})
+
+header.addEventListener('click', e => {
+    currClick = e.target;
+    checkClass(currClick);
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 600) {
+        menuLink[4].innerHTML = 'MERCH';
+    } else {
+        menuLink[4].innerHTML = 'MORE<i class="menu__dropdown-icon fa-solid fa-caret-down"></i>';
+    }
+})
--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,6 +1,7 @@
 const overlay = document.getElementById('overlay'),
     modalWindow = document.getElementById('modalWindow'),
-    tourTickets = document.getElementById('tourTickets');
+    tourTickets = document.getElementById('tourTickets'),
+    body = document.querySelector('body');
 
 const crossBtn = document.getElementById('crossBtn'),
       closeBtn = document.getElementById('closeBtn');
@@ -11,12 +12,14 @@ const check = () => {
     if (currBtn.classList.contains('tour-tickets__btn')) {
         overlay.classList.toggle('overlay--active')
         modalWindow.classList.toggle('ticket-modal--active')
+        body.style.overflow = "hidden";
     }
 }
 
 const removeClass = () => {
     overlay.classList.toggle('overlay--active');
     modalWindow.classList.toggle('ticket-modal--active');
+    body.style.overflow = "auto";
 }
 
 tourTickets.addEventListener('click', e => {
